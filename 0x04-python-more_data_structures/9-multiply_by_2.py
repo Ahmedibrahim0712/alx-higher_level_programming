dbafb10b80e718cbb70298c186df1ac53eab1b99
MonteBurns1 <zx998@outlook.sa>
2024-01-11 23:57:42 +0200

    asass

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dir = a_dictionary.copy()
-    list_keys = list(new_dir.keys())
-
-    for i in list_keys:
-        new_dir[i] *= 2
-
-    return (new_dir)
+    newC = a_dictionary.copy()
+    for i, j in newC.items():
+        newC[i] = j * 2
+    return (newC)
