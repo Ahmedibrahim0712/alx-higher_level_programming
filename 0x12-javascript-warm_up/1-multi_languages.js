08787bb0e1bc2d2fd9ac1dd44a7f473910bc9fb7
MonteBurns1 <zx998@outlook.sa>
2024-04-21 04:46:34 +0200

    assa

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -7,6 +7,4 @@ The second line: “Python is cool”
 The third line: “Javascript is amazing”
 */
 
-console.log('C is fun');
-console.log('Python is cool');
-console.log('Javascript is amazing');
+console.log('C is fun\nPython is cool\nJavascript is amazing');
